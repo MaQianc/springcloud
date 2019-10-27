8f77e38b74cd97e0992ea2dc983bb5dbc8b3cb30
maqianfd <maqianfd@163.com>
2019-10-27 21:36:16 +0800

    test

--- spring-cloud-consul-consumer/src/main/java/com/mq/springcloud/consul/demo/SpringCloudConsulConsumerApplication.java
+++ spring-cloud-consul-consumer/src/main/java/com/mq/springcloud/consul/demo/SpringCloudConsulConsumerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class SpringCloudConsulConsumerApplication {
 
 	public static void main(String[] args) {
-		System.out.print("123");
+		System.out.print("123258");
 		SpringApplication.run(SpringCloudConsulConsumerApplication.class, args);
 	}
 
